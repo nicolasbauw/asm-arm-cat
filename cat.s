3af4277c620cfff1de5099d8217b578ab86a4277
Nicolas BAUW <nbauw@hotmail.com>
2024-02-01 15:36:22 +0100

    Error handling

--- cat.s
+++ cat.s
@@ -15,6 +15,8 @@ _start:
     mov r2,#2           @ SEEK_END
     mov r7,#0x13        @ "lseek" system call
     svc #0
+    cmp r0,#0           @ Error ? we close the FD and exit
+    blt close_exit
     mov r6,r0           @ we save the file size in r6
 
     mov r0,r5           @ first argument : the file descriptor (saved in r5)
@@ -22,6 +24,8 @@ _start:
     mov r2,#0           @ SEEK_SET
     mov r7,#0x13        @ "lseek" system call
     svc #0
+    cmp r0,#0           @ Error ? we close the FD and exit
+    blt close_exit
 
     mov r0,#1           @ Output to stdout (file descriptor 1)
     mov r1,r5           @ Input = the FD we opened (saved in r5)
@@ -30,7 +34,8 @@ _start:
     mov r7,#0xbb        @ "sendfile" system call
     svc #0
 
-    mov r0,r1           @ file descriptor was saved in r1
+close_exit:
+    mov r0,r5           @ file descriptor was saved in r5
     mov r7,#6           @ "close" system call
     svc #0
 
